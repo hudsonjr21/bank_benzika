cbaf4ec254db8b772a5a158880b0d3cd8ec7acda
hudsonjr21 <hudsonferreiramb@hotmail.com>
2023-08-25 17:54:58 -0300

    fix server

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,9 @@
-import express, { Request, Response, NextFunction } from 'express';
+import express, { Request, Response, NextFunction } from 'express'
 import 'express-async-errors';
 import cors from 'cors';
-import path from 'path';
-import { router } from './routes';
+import path from 'path'
 import { PrismaClient } from '@prisma/client'; // Importe o Prisma Client
+import { router } from './routes'
 
 const prisma = new PrismaClient(); // Inicialize o Prisma Client
 
@@ -16,21 +16,23 @@ app.use(router);
 app.use(
   '/files',
   express.static(path.resolve(__dirname, '..', 'tmp'))
-);
+)
 
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
-  if (err instanceof Error) {
+  if(err instanceof Error){
+    //Se for uma instancia do tipo error
     return res.status(400).json({
-      error: err.message,
-    });
+      error: err.message
+    })
   }
 
   return res.status(500).json({
     status: 'error',
-    message: 'Internal server error.',
-  });
-});
+    message: 'Internal server error.'
+  })
 
-export default app; // Exporte o app
+})
 
-// Remova o app.listen() aqui, pois a Vercel irá gerenciar o servidor
+export default app; 
+
+// app.listen(3333, () => console.log('Servidor online!!!!'));
